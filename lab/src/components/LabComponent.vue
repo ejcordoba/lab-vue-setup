033fd2cc3373f44d25e389d91ae2e190d8ff4572
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2022-12-09 13:16:34 +0100

    iterations 1 to 3 done, TO-DO: iteration 4 after router lesson

---
+++ lab/src/components/LabComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        {{ message }}<br />
+        <button type="button" @click="showLog">Here's a button that show up a console log, check it out!</button>
+    </div>
+</template>
+
+<script setup>
+const message = 'This is my custom component just created!';
+function showLog() {
+    console.log('Hello World!');
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
